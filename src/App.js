c77a74b22b8935ad64eee9984f461642da8fa154
Natalie Bale <natalia.balakhashvili@geolab.edu.ge>
2023-10-05 19:04:32 +0400

    feat - Board & Game components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, {useState} from 'react';
 import Square from "./components/Square";
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+import Board from "./components/Board";
 
-  function handleClick(i) {
-    if(squares[i]) return;
-
-    const nextSquares = squares.slice();
-
-    nextSquares[i] = xIsNext ? 'X' : 'O';
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
-  }
-
+export default function Game() {
   return (
-    <div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+    <div className="game">
+      <div className="game-board">
+        <Board></Board>
       </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
+      <div className="game-info">
+        <ol>
+          list
+        </ol>
       </div>
     </div>
   );
