34a30930229f1e86959c8ed126ad6b0d2e437339
Natalie Bale <natalia.balakhashvili@geolab.edu.ge>
2023-10-05 11:52:46 +0400

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, {useState} from 'react';
 import Square from "./components/Square";
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if(squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
 
